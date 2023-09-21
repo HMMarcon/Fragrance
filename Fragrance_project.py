f0489c031537ca215d141c0c736123f433c9ca06
HMMarcon <94055184+HMMarcon@users.noreply.github.com>
2023-09-21 11:03:45 +0100

    Update Fragrance_project.py

--- Fragrance_project.py
+++ Fragrance_project.py
@@ -59,6 +59,7 @@ st.markdown("# Draw your molecule")
 st.markdown("Draw your molecule and click on the button below to get the SMILES code")
 smile_code = st_ketcher()
 st.markdown(f"Smile code: ``{smile_code}``")
+st.markdown(smiles_code)
 
 #compound = Chem.MolFromSmiles(smiles_code)
 compound_FP = smiles_to_fp([smiles_code])
