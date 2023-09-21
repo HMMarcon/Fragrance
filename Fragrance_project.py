fb0eb9e6353acb9789cf9479f4b0f1f3767912a2
HMMarcon <94055184+HMMarcon@users.noreply.github.com>
2023-09-21 10:50:55 +0100

    Update Fragrance_project.py
    Drawing to SMILES is working
    Test SMILES to FP

--- Fragrance_project.py
+++ Fragrance_project.py
@@ -24,8 +24,45 @@ from xgboost import XGBClassifier
 from streamlit_ketcher import st_ketcher
 
 
+# Fingerprints functions
+fp_size = [2048]
+fp_radius = [2]
+
+
+def smiles_to_fp(smiles_list):
+    fp_list = []
+    for smile in smiles_list:
+        fp_list.append(fp_as_array(smile))
+    return fp_list
+
+
+def fp_as_array(smile):
+    mol = Chem.MolFromSmiles(smile)
+    fp = AllChem.GetMorganFingerprintAsBitVect(mol, fp_radius[0], nBits=fp_size[0])
+    arr = np.zeros((1,), int)
+    DataStructs.ConvertToNumpyArray(fp, arr)
+    return arr
+'''
+## Loading models
+@st.cache_resource
+def load_models():
+    return pickle.load(open("model_fp2fp_Ridge_7.5.mlpickle", "rb"))
+
+
+
+# svd2svd_models = load_models()
+# svd = load_svd()
+fp2fp_model = load_models()
+'''
+
 st.markdown("# Draw your molecule")
 st.markdown("Draw your molecule and click on the button below to get the SMILES code")
 smile_code = st_ketcher()
-st.markdown(f"Smile code: ``{smile_code}``")
+#st.markdown(f"Smile code: ``{smile_code}``")
+
+compound = Chem.MolToSmiles(smiles_code)
+compound_FP = smiles_to_fp([compound])
+
+st.markdown(compound_FP)
+
 
